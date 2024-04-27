20a972982656b15355f78552f74e901fdebfebfe
mohamedmehdigara <garamehdi57@yahoo.com>
2024-04-27 11:38:08 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
@@ -6,14 +6,42 @@ import CourseList from './components/CourseList';
 import CourseDetail from './components/CourseDetail'; // Import CourseDetail component
 import Certifications from './components/Certifications';
 import UserProfile from './components/UserProfile';
+import Search from './components/Search';
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import './App.css';
 
 const App = () => {
+
+  const [searchResults, setSearchResults] = useState([]);
+
+  const courses = [
+    {
+      id: 1,
+      title: "Introduction to JavaScript",
+      description: "Learn the basics of JavaScript programming language.",
+      // Other properties
+    },
+    // Other course objects
+  ];
+
+  // Function to handle search
+  const handleSearch = (searchTerm) => {
+    // Filter courses based on the search term
+    const filteredCourses = courses.filter(course =>
+      course.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      course.description.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  
+    // Update searchResults with filtered courses
+    setSearchResults(filteredCourses);
+  };
+  
   return (
     <div>
       <Router>
         <Header/>
+        <Search onSearch={handleSearch} />
+
         
         <Routes>
           <Route path="/" element={<Main />} />
